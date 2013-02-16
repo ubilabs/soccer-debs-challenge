fef9ec606a8c53b4f08568ac9df788654f0aaa9a
Martin Kleppe <kleppe@ubilabs.net>
2013-02-16 15:38:54 +0100

    Add main app.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,61 +1,99 @@
-var stats = new FPSCounter(),
-  scene = new Scene();
+App = Model({
 
-animate();
+  $time: $("#time"),
 
-var $time = $("#time");
+  cache: {},
+  index: 0,
+  count: 0,
 
-var cache = {},
-  index = 0,
-  count = 0,
-  lines;
+  init: function(){
+    this.stats = new FPSCounter();
+    this.scene = new Scene();
 
-var loader = new Loader();
+    this.render();
+    this.load();
+  },
 
-loader.on("loaded", function(data){
-  lines = data;
-  run();
+  load: function(){
+    var loader = new Loader();
+    loader.on("loaded", this.loaded);
+  },
+
+  loaded: function(data){
+    this.lines = data;
+    this.run();
+  },
+
+  getPlayer: function(id){
+    var player = this.cache[id];
+
+    if (!player){
+      player = this.cache[id] = new Player({
+        scene: this.scene,
+        id: id
+      });
+    }
+
+    return player;
+  },
+
+  run: function(){
+
+    var that = this;
+
+    function run(){
+
+      that.index++;
+
+      var data = that.lines[that.index];
+
+      if (!data) { return; }
+
+      data = data.split(",");
+
+      var id = 1 * data[0],
+        player = that.getPlayer(id);
+
+      player.update(data);
+
+      if (id == 4){
+        //closest();
+      }
+
+      that.time = data[1];
+
+      that.count++;
+
+      if (that.count < 2000){
+        run();
+      } else {
+        that.count = 0;
+        requestAnimationFrame(run);
+      }
+    }
+
+    run();
+
+
+  },
+
+
+  render: function(){
+    requestAnimationFrame( this.render );
+    this.scene.update();
+    this.stats.update();
+
+    if (this.time){
+      var time = (this.time - START) / 1e12;
+      this.$time.html(
+        Math.floor(time/60) + ":" + Math.round(time % 60)
+      );
+    }
+  }
 });
 
-function run(){
-  
-  var line, data, id, player;
+app = new App();
 
-  line = lines[index++];
-
-  if (!line) { return; }
-
-  data = line.split(",");
-  id = 1 * data[0];
-  player = cache[id];
-
-  if (!player){
-    cache[id] = player;
-    player = new Player({
-      scene: scene,
-      id: id
-    });
-
-    cache[id] = player;
-  }
-
-  player.update(data);
-
-  if (id == 4){
-    closest();
-  }
-
-  count++;
-
-  if (count < 2000){
-    run();
-  } else {
-    count = 0;
-    var time = (data[1]-START) / 1e12;
-    $time.html(Math.floor(time/60) + ":" + Math.round(time % 60));
-    requestAnimationFrame(run);
-  }
-}
 
 function closest(){
 
@@ -96,9 +134,3 @@ function closest(){
   }
 }
 
-function animate() {
-  requestAnimationFrame( animate );
-
-  scene.update();
-  stats.update();
-}

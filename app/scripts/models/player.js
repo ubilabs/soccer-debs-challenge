5d70d80974a3864132369bc366e3dd5c07550b88
Martin Kleppe <kleppe@ubilabs.net>
2013-04-19 17:44:15 +0200

    Use current speed instead of average.

--- app/scripts/models/player.js
+++ app/scripts/models/player.js
@@ -100,7 +100,7 @@ GLOBAL.Player = Klass({
     speed = computeSpeed(distance, diff);
 
     for (var i in SPEED){
-      if ((1*i) < this.speed){
+      if ((1*i) < speed){
         type = i;
       }
     }

bf224ce358ab7e88c9b41ad142dcba20aa8861ea
Martin Kleppe <kleppe@ubilabs.net>
2013-04-20 01:31:48 +0200

    Show ETA on command line.

--- app/scripts/models/loader.js
+++ app/scripts/models/loader.js
@@ -26,16 +26,37 @@ GLOBAL.Loader = Model({
 
   loadInNode: function(){
     var fs = require('fs'),
+      sys = require('sys'),
       input = fs.createReadStream(this.file),
       callback = function(data){
         this.trigger("data", data);
       }.bind(this),
-      last;
+      last,
+      count = 0,
+      time = 0,
+      start = new Date();
 
     input.on('data', function(data, lines) {
       lines = (data + "").split("\n");
       if (last){ lines[0] = last + lines[0]; }
       last = lines.pop();
+
+      count += lines.length;
+
+      if ((new Date() - time) > 1000){
+        time = new Date();
+
+        var seconds = (new Date() - start) / 1000,
+          portion = count / 49576080;
+
+        sys.print(
+          "\r" +
+          (portion * 100).toFixed(1) + "% - " +
+          Math.round(seconds) + "s - ETA: " +
+          Math.round(seconds * 1 / portion) + "s"
+        );
+      }
+
       callback(lines);
     });
 

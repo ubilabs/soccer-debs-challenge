f3d9f694b607de7dbf04f27ac9a20dcc12819e60
Martin Kleppe <kleppe@ubilabs.net>
2013-04-20 01:30:32 +0200

    Add stream for goal target.

--- app/scripts/models/target.js
+++ app/scripts/models/target.js
@@ -76,8 +76,22 @@ GLOBAL.Target = Klass({
       goal.hit
     );
 
-    if (GAME.shot){
-      console.log("shot");
+    if (GAME.shot && !IS_BROWSER){
+      write("shot_on_goal", [
+        data[1],
+        GAME.current.player.name,
+        data[2],
+        data[3],
+        data[4],
+        data[5],
+        data[6],
+        data[7],
+        data[8],
+        data[9],
+        data[10],
+        data[11],
+        data[12]
+      ]);
     }
 
     if (IS_BROWSER){
